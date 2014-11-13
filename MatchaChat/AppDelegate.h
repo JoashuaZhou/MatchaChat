159887e75758f0748e311f6215e751bb2f5b76ff
a226035102 <comeonjiji@gmail.com>
2014-11-13 12:02:09 +0800

    Initial Commit

---
+++ MatchaChat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MatchaChat
+//
+//  Created by Joshua Zhou on 14/11/13.
+//  Copyright (c) 2014年 Joshua Zhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
